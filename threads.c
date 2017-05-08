1afe71259029be0c624cbc88e18c3f2bc1c213b0
Joshua Owens <sadpuppy@grace1.umd.edu>
2017-05-07 21:11:24 -0400

    super finished

--- threads.c
+++ threads.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <string.h>
 
 typedef struct argument {
 	int * arr;
@@ -41,6 +44,19 @@ void * calc_sum(void * void_arg){
 	return NULL;
 }
 
+struct timeval tv_delta(struct timeval start, struct timeval end) {
+   struct timeval delta = end;
+
+   delta.tv_sec -= start.tv_sec;
+   delta.tv_usec -= start.tv_usec;
+   if (delta.tv_usec < 0) {
+      delta.tv_usec += 1000000;
+      delta.tv_sec--;
+   }
+
+   return delta;
+}
+
 int main(int args, char* argv[]){
 	int size = atoi(argv[1]), chunk;
 	int threads = atoi(argv[2]);
@@ -49,8 +65,13 @@ int main(int args, char* argv[]){
 	int * array = malloc(sizeof(int)*size), i;
 	Argument * arg;
 	pthread_t * tids = malloc(sizeof(pthread_t)*threads);
+   struct rusage start_ru, end_ru;
+   struct timeval start_wall, end_wall;
+   struct timeval diff_ru_utime, diff_wall, diff_ru_stime;
+   
+	getrusage(RUSAGE_SELF, &start_ru);
+	gettimeofday(&start_wall, NULL);
 	
-	printf("%d %d %d %d %s\n", size, threads, seed, task, argv[5]);
 	srand(seed);
 	pthread_mutex_init(&mutex, NULL);
 	
@@ -59,7 +80,6 @@ int main(int args, char* argv[]){
 	}
 	
 	chunk = size/threads;
-	
 	if(task == 1){
 		for(i = 0; i < threads; i++){
 			arg = malloc(sizeof(Argument));
@@ -74,7 +94,7 @@ int main(int args, char* argv[]){
 			pthread_join(tids[i], NULL);
 		}
 		
-		printf("%d\n", max);
+		printf("Max: %d\n", max);
 	} else if(task == 2){
 		for(i = 0; i < threads; i++){
 			arg = malloc(sizeof(Argument));
@@ -89,8 +109,21 @@ int main(int args, char* argv[]){
 			pthread_join(tids[i], NULL);
 		}
 		
-		printf("%lu\n", sum);
+		printf("Sum: %lu\n", sum);
 	}
 	pthread_mutex_destroy(&mutex);
+	
+	if(!strcmp(argv[5], "Y")){
+		gettimeofday(&end_wall, NULL);
+		getrusage(RUSAGE_SELF, &end_ru);
+		diff_ru_utime = tv_delta(start_ru.ru_utime, end_ru.ru_utime);
+		diff_ru_stime = tv_delta(start_ru.ru_stime, end_ru.ru_stime);
+		diff_wall = tv_delta(start_wall, end_wall);
+
+		printf("User time: %ld.%06ld\n", diff_ru_utime.tv_sec, diff_ru_utime.tv_usec);
+		printf("System time: %ld.%06ld\n", diff_ru_stime.tv_sec, diff_ru_stime.tv_usec);
+		printf("Wall time: %ld.%06ld\n", diff_wall.tv_sec, diff_wall.tv_usec);
+	}
+	
 	return 0;
 }
